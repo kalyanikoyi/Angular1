f9824033b0a7ab51edf698c967a4e7af47dc69da
kalyanikoyi <kalyanikoyi143@gmail.com>
2023-02-06 14:17:02 +0530

    two way binding

---
+++ src/app/Binding/TwoWayBinding/twoway-binding/twoway-binding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-twoway-binding',
+  templateUrl: './twoway-binding.component.html',
+  styleUrls: ['./twoway-binding.component.css']
+})
+export class TwowayBindingComponent {
+name = ''
+}
